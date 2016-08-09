4968daed1a6f8ab0546ec39a7cc6553e0d8e052e
Mathias <201406659@post.au.dk>
2016-08-09 22:18:25 +0200

    Created project for conjoin website

---
+++ Conjoin/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Conjoin.Models;
+
+namespace Conjoin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProductDescription()
+        {
+
+
+
+            ViewBag.Foo = "Produkter.";
+
+            return View();
+        }
+
+        public JsonResult GetData(Product product)
+        {
+            // Insert data product from json object
+            return Json(product.Name, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Conjoin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Conjoin.MvcApplication" Language="C#" %>
